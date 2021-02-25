e934ba339420e9ced46a66a8b512b80128f220ad
Nikita <35899038+NikitaCh0312@users.noreply.github.com>
2021-02-25 13:45:53 +0700

    Create DescriptionApiHandler.h

---
+++ DispenserHAL_v1.0/Project/HighLvl/modules/api/DescriptionApiHandler.h
@@ -0,0 +1,29 @@
+#ifndef DESCRIPTION_API_HANDLER_H_
+#define DESCRIPTION_API_HANDLER_H_
+
+
+
+
+class DescriptionApiHandler: public ApiHandler
+{
+public:
+  
+  DescriptionApiHandler()
+  {
+    
+  }
+  
+  void Handle()
+  {
+    
+  }
+  
+private:
+}
+
+
+
+
+
+
+#endif
