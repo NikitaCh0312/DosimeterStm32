1b5835f1c12ec69325d6056637994cc902695a12
Nikita <35899038+NikitaCh0312@users.noreply.github.com>
2021-02-25 13:48:25 +0700

    Chzhen/Added EventLogApiHandler

---
+++ DispenserHAL_v1.0/Project/HighLvl/modules/api/EventLogApiHandler.h
@@ -0,0 +1,27 @@
+#ifndef EVENT_LOG_API_HANDLER_H_
+#defien EVENT_LOG_API_HANDLER_H_
+
+
+
+class EventLogApiHandler: public ApiHandler
+{
+public:
+    EventLogApiHandler()
+    {
+      
+    }
+    virtual ~EventLogApiHandler(){}
+  
+    void Handle()
+    {
+      
+    }
+private:
+};
+
+
+
+
+
+
+#endif
