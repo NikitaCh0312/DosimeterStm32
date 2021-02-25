8e2f280a546f4affd61878c723ea0d2176fc816a
Nikita <35899038+NikitaCh0312@users.noreply.github.com>
2021-02-25 15:26:15 +0700

    Update EthernetInit.h

--- DispenserHAL_v1.0/Project/Components/Ethernet/EthernetInit.h
+++ DispenserHAL_v1.0/Project/Components/Ethernet/EthernetInit.h
@@ -2,8 +2,23 @@
 #define ETHERNET_INIT_H_
 
 
+typedef struct
+{
+  uint8_t ipv4_1;
+  uint8_t ipv4_2;
+  uint8_t ipv4_3;
+  uint8_t ipv4_4;
+}IPV4_t;
 
-void EthernetInit();
+typedef struct
+{
+  uint8_t mask1;
+  uint8_t mask2;
+  uint8_t mask3;
+  uint8_t mask4; 
+}MASK_t;
+
+void EthernetInit(IPV4_t ip_addr, MASK_t mask);
 
 
 #endif
