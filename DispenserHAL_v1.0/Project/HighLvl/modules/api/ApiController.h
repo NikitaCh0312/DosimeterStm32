3b68be0f8175b0e80658168b8c59a4a672d50b8a
Nikita <35899038+NikitaCh0312@users.noreply.github.com>
2021-02-25 13:38:05 +0700

    Chzhen/Added ApiController

---
+++ DispenserHAL_v1.0/Project/HighLvl/modules/api/ApiController.h
@@ -0,0 +1,43 @@
+#ifndef API_CONTORLLER_H_
+#define API_CONTORLLER_H_
+
+
+
+class ApiController
+{
+public:
+    ApiController()
+    {
+      
+    }
+  
+    virtual ~ApiController(){} 
+  
+    void RegisterHandler()
+    {
+      
+    }
+  
+    Handle()
+    {
+      
+    }
+private:
+    dictionary _apiHandlers;
+  
+    char * GetResourceName()
+    {
+      
+    }
+  
+    void GetHttpMethodType()
+    {
+      
+    }
+};
+
+
+
+
+
+#endif
