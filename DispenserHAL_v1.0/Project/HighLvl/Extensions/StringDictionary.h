782f801664bb289b1169a2b55546ebe741b7ae72
Nikita Chzhen <nch2591@gmail.com>
2022-10-02 18:33:31 +0700

    qqq

--- DispenserHAL_v1.0/Project/HighLvl/Extensions/StringDictionary.h
+++ DispenserHAL_v1.0/Project/HighLvl/Extensions/StringDictionary.h
@@ -11,10 +11,10 @@ class StringDictionary
 public:
     StringDictionary()
     {
-		for (int i = 0; i < DICTIONARY_MAX_VALUES; i++)
-		{
-			_values[i].key[0] = '\0';
-		}
+        for (int i = 0; i < DICTIONARY_MAX_VALUES; i++)
+        {
+                _values[i].key[0] = '\0';
+        }
     }
     virtual ~StringDictionary(){}
     
@@ -26,13 +26,13 @@ public:
     
     bool TryGetValue(char * key, T * outValue)
     {
-		int position = GetHashCode_2(key);
-		if (!strcmp(key, _values[position].key))
-		{
-			*outValue = _values[position].value;
-			return true;
-		}
-		return false;
+        int position = GetHashCode_2(key);
+        if (!strcmp(key, _values[position].key))
+        {
+                *outValue = _values[position].value;
+                return true;
+        }
+        return false;
     }
     
     bool TryAddValue(char * key, T value)
@@ -42,10 +42,10 @@ public:
 		
         int position = GetHashCode_2(key);
 		
-		if (strlen(_values[position].key) != 0)
-			return false;
+        if (strlen(_values[position].key) != 0)
+                return false;
 		
-		strcpy(_values[position].key, key);
+	strcpy(_values[position].key, key);
         _values[position].value = value;
 		
 		return true;
@@ -55,31 +55,31 @@ private:
 	//unused
 	int GetHashCode_1(char * key)
 	{
-		int value = ConvertStringToInt(key);
-		return value % DICTIONARY_MAX_VALUES;
+            int value = ConvertStringToInt(key);
+            return value % DICTIONARY_MAX_VALUES;
 	}
 	
 	int GetHashCode_2(char * key)
 	{
-		int value = ConvertStringToInt(key);
-		
-		float full_value = (float)value * HASH_FUNCTION_KOEFFICIENT;
-		
-		int whole = (int)full_value;
+            int value = ConvertStringToInt(key);
+            
+            float full_value = (float)value * HASH_FUNCTION_KOEFFICIENT;
+            
+            int whole = (int)full_value;
 
-		float fractional = full_value - (float)whole;
+            float fractional = full_value - (float)whole;
 
-		return fractional * DICTIONARY_MAX_VALUES;
+            return fractional * DICTIONARY_MAX_VALUES;
 	}
 	
 	int ConvertStringToInt(char * str)
 	{
-		int value = 0;
-		for (int i = 0; i < strlen(str); i++)
-		{
-			value += (int)str[i];
-		}
-		return value;
+            int value = 0;
+            for (int i = 0; i < strlen(str); i++)
+            {
+                    value += (int)str[i];
+            }
+            return value;
 	}
 	
     DictionaryValue_t _values[DICTIONARY_MAX_VALUES];
