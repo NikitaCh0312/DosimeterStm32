742abfbaeb58ae7be6d460148f1c759ed7bd8c84
Igor Erakhtin <erakhtin.i.k@gmail.com>
2022-11-28 23:10:54 +0700

    added enum EventID_List

--- DispenserHAL_v1.0/Project/HighLvl/modules/Storage/EventJournalStorage.h
+++ DispenserHAL_v1.0/Project/HighLvl/modules/Storage/EventJournalStorage.h
@@ -25,6 +25,43 @@ typedef struct
     uint16_t Crc;
 }EventJournalRecord_t;
 
+typedef enum
+{
+    Event_0R1 = 0,      // device configuration is OK 
+    Event_2R1,          // pwr voltage is bad
+    Event_2R2,          // substance is no in tank  
+    Event_2R3,          // insufficient water pressure in the pipeline
+    
+    Event_3R1,          // the "xxxx" card has been identified, not attached to this Device
+    Event_3R2,          // the inactive "xxxx" card has been identified:x"
+    Event_3R3,          // the extended access card "xxx"
+    Event_3R4,          // has been identified - the current task card 
+    Event_3R5,          // has been identified - work with the "xxxx" card has been terminated due to a failure
+    Event_3R6,          // work with the "xxxx" card has been completed successfully: all tasks have been completed 
+    Event_3R7,          // work with the 270F card has been completed successfully: tasks 'n'
+    Event_3R8,          // task 'n' cancelled during execution
+    Event_3R9,          // task card "xxxx" cancelled. In total, 'n' tasks have not been completed
+    
+    Event_4R1,          //system date successfully updated
+    Event_4R2,          //network connection settings changed successfully
+    Event_4R3,          //forced release of solution started 
+    Event_4R4,          //forced release of solution completed successfully
+    Event_4R5,          //forced release of solution interrupted
+    Event_4R6,          //the procedure for cleaning the SCP from the drug has been started
+    Event_4R7,          //the procedure for cleaning the SCP from the drug has been completed successfully
+    Event_4R8,          //the procedure for cleaning the SCP from the drug has been interrupted without consequences
+    Event_4R9,          //the procedure for washing the SCP has been started
+    Event_4R10,         //the procedure for flushing the SCP was completed successfully
+    Event_4R11,         //the procedure for refueling the fresh drug was canceled
+    Event_4R12,         //the procedure for refueling the fresh drug was performed successfully
+    Event_4R13,         //the procedure for selecting the current drug was performed successfully
+
+    Event_5R7,          //received a request to lock the Device
+    Event_5R8,          //the lock was completed successfully
+    Event_5R12,         //UT sent a new time. Correction of the system time was carried out successfully
+    Event_5R19          //received a request to send the first block
+}EventID_List_t;
+
 
 class EventJournalStorage : BaseStorage
 {
